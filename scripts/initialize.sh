bca2762f4bf4608c1f7c1aeef11054306a20c138
Obsec24 <obsecpolitecnico@gmail.com>
2025-11-21 11:55:32 -0500

    Linea agregada para que mitmproxy arranque
    Se agrego esto porque nunca arrancaba por ello no capturaba nada. Con el nuevo comando arranca y guarda logs del mismo.

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
+
+# Esperar a que el contenedor arranque bien
+sleep 2
+
+# Iniciar mitmproxy con log permanente
+mitmdump -s /app/intercept-master/inspect_requests.py \
+         --listen-port 8080 \
+         --set app=ALL \
+         --set block_global=false \
+         > /app/intercept-master/mitm.log 2>&1 &
+
+# Iniciar control-master
 ./control-master traffic 4000 &
+
+# Iniciar el executor
 python3 ./executor-master/queue_receive.py
+
 
 
